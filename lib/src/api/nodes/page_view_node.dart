d9880a4ff47bbf63ceebcc7171a8fe2da3a5cf97
birjuvachhani <brvachhani@gmail.com>
2023-04-07 16:36:41 +0530

    PageView #1
    - Add page view node.

---
+++ lib/src/api/nodes/page_view_node.dart
@@ -0,0 +1,167 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+import '../../../codelessly_api.dart';
+
+part 'page_view_node.g.dart';
+
+/// Page View is a widget that lets the user create carousel like scrolling
+/// experience.
+/// A scrollable list that works page by page.
+/// Each child of a page view is forced to be the same size as the viewport.
+@JsonSerializable()
+class PageViewNode extends SinglePlaceholderNode
+    with
+        ScrollableMixin,
+        CustomPropertiesMixin,
+        VariablePropertiesMixin,
+        ClipMixin,
+        IsolatedMixin {
+  @override
+  final String type = 'pageView';
+
+  /// The properties of the [PageViewNode].
+  PageViewProperties properties;
+
+  @override
+  bool get supportsPadding => false;
+
+  /// Creates a [PageViewNode].
+  PageViewNode({
+    required super.id,
+    required super.name,
+    required super.basicBoxLocal,
+    super.outerBoxLocal,
+    super.retainedOuterBoxLocal,
+    super.visible,
+    super.rotationDegrees,
+    super.alignment,
+    super.margin,
+    super.padding,
+    super.horizontalFit,
+    super.verticalFit,
+    super.flex,
+    super.constraints,
+    super.edgePins,
+    super.aspectRatioLock,
+    super.positioningMode,
+    super.parentID,
+    super.reactions,
+    required this.properties,
+    required List<String> children,
+    Map<String, String>? variables,
+    bool clipsContent = true,
+    // scrollable
+    AxisC scrollDirection = AxisC.vertical,
+    bool reverse = false,
+    ScrollPhysicsC physics = ScrollPhysicsC.alwaysScrollableScrollPhysics,
+    bool primary = false,
+    ScrollViewKeyboardDismissBehaviorC keyboardDismissBehavior =
+        ScrollViewKeyboardDismissBehaviorC.manual,
+  }) : super(
+          children: [],
+          allowedTypes: [],
+          deniedTypes: ['pageView'],
+        ) {
+    setChildrenMixin(children: children);
+    setVariablesMixin(variables: variables);
+    setClipMixin(clipsContent: clipsContent);
+
+    setScrollableMixin(
+      isScrollable: true,
+      scrollDirection: scrollDirection,
+      reverse: reverse,
+      physics: physics,
+      primary: primary,
+      shrinkWrap: false,
+      keyboardDismissBehavior: keyboardDismissBehavior,
+      useFlutterListView: true,
+    );
+  }
+
+  /// Creates a [ListViewNode] from a JSON object.
+  factory PageViewNode.fromJson(Map json) => _$PageViewNodeFromJson(json);
+
+  @override
+  Map toJson() => _$PageViewNodeToJson(this);
+
+  /// [returns] the appropriate alignment of the child based on the
+  /// [scrollDirection] and [reverse] properties.
+  AlignmentModel childAlignment() {
+    switch (scrollDirection) {
+      case AxisC.horizontal:
+        return reverse ? AlignmentModel.centerRight : AlignmentModel.centerLeft;
+      case AxisC.vertical:
+        return reverse ? AlignmentModel.bottomCenter : AlignmentModel.topCenter;
+    }
+  }
+}
+
+/// The properties of a [PageViewNode].
+@JsonSerializable()
+class PageViewProperties with EquatableMixin, SerializableMixin {
+  /// The number of items to display in the list view. Can be null if the list
+  /// is infinite.
+  int? itemCount;
+
+  /// Set to false to disable page snapping, useful for custom scroll behavior.
+  bool pageSnapping;
+
+  /// Whether to add padding to both ends of the list.
+  bool padEnds;
+
+  /// Keep current page in memory and restore it when the page is recreated.
+  bool keepPage;
+
+  /// The page to show when first creating the Page View.
+  int initialPage;
+
+  /// The fraction of the viewport that each page should occupy.
+  double viewportFraction;
+
+  /// Creates a [PageViewProperties].
+  PageViewProperties({
+    this.itemCount,
+    this.pageSnapping = true,
+    this.padEnds = true,
+    this.keepPage = true,
+    this.initialPage = 0,
+    this.viewportFraction = 1,
+  });
+
+  /// Creates a [PageViewProperties] from a JSON object.
+  PageViewProperties copyWith({
+    int? itemCount,
+    bool? pageSnapping,
+    bool? padEnds,
+    bool? keepPage,
+    int? initialPage,
+    double? viewportFraction,
+  }) {
+    return PageViewProperties(
+      itemCount: itemCount ?? this.itemCount,
+      pageSnapping: pageSnapping ?? this.pageSnapping,
+      padEnds: padEnds ?? this.padEnds,
+      keepPage: keepPage ?? this.keepPage,
+      initialPage: initialPage ?? this.initialPage,
+      viewportFraction: viewportFraction ?? this.viewportFraction,
+    );
+  }
+
+  @override
+  List<Object?> get props => [
+        itemCount,
+        pageSnapping,
+        padEnds,
+        keepPage,
+        initialPage,
+        viewportFraction,
+      ];
+
+  @override
+  Map toJson() => _$PageViewPropertiesToJson(this);
+
+  /// Creates a [PageViewProperties] from a JSON object.
+  factory PageViewProperties.fromJson(Map json) =>
+      _$PageViewPropertiesFromJson(json);
+}
---
+++ lib/src/api/nodes/page_view_node.g.dart
@@ -0,0 +1,190 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'page_view_node.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PageViewNode _$PageViewNodeFromJson(Map json) => PageViewNode(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      basicBoxLocal: NodeBox.fromJson(json['basicBoxLocal'] as Map),
+      outerBoxLocal: json['outerBoxLocal'] == null
+          ? null
+          : OuterNodeBox.fromJson(json['outerBoxLocal'] as Map),
+      visible: json['visible'] as bool? ?? true,
+      rotationDegrees:
+          json['rotation'] == null ? 0 : castRotation(json['rotation']),
+      alignment: json['alignment'] == null
+          ? AlignmentModel.none
+          : AlignmentModel.fromJson(json['alignment'] as Map),
+      margin: json['margin'] == null
+          ? EdgeInsetsModel.zero
+          : EdgeInsetsModel.fromJson(json['margin'] as Map),
+      padding: json['padding'] == null
+          ? EdgeInsetsModel.zero
+          : EdgeInsetsModel.fromJson(json['padding'] as Map),
+      horizontalFit:
+          $enumDecodeNullable(_$SizeFitEnumMap, json['horizontalFit']) ??
+              SizeFit.fixed,
+      verticalFit: $enumDecodeNullable(_$SizeFitEnumMap, json['verticalFit']) ??
+          SizeFit.fixed,
+      flex: json['flex'] as int? ?? 1,
+      constraints: json['constraints'] == null
+          ? const BoxConstraintsModel()
+          : BoxConstraintsModel.fromJson(json['constraints'] as Map),
+      edgePins: json['edgePins'] == null
+          ? EdgePinsModel.standard
+          : EdgePinsModel.fromJson(
+              Map<String, dynamic>.from(json['edgePins'] as Map)),
+      aspectRatioLock: json['aspectRatioLock'] as bool? ?? false,
+      positioningMode: $enumDecodeNullable(
+              _$PositioningModeEnumMap, json['positioningMode']) ??
+          PositioningMode.align,
+      reactions: (json['reactions'] as List<dynamic>?)
+              ?.map((e) => Reaction.fromJson(e as Map))
+              .toList() ??
+          const [],
+      properties: PageViewProperties.fromJson(json['properties'] as Map),
+      children:
+          (json['children'] as List<dynamic>).map((e) => e as String).toList(),
+      variables: (json['variables'] as Map?)?.map(
+            (k, e) => MapEntry(k as String, e as String),
+          ) ??
+          {},
+      clipsContent: json['clipsContent'] as bool? ?? true,
+      scrollDirection:
+          $enumDecodeNullable(_$AxisCEnumMap, json['scrollDirection']) ??
+              AxisC.vertical,
+      reverse: json['reverse'] as bool? ?? false,
+      physics: $enumDecodeNullable(_$ScrollPhysicsCEnumMap, json['physics']) ??
+          ScrollPhysicsC.alwaysScrollableScrollPhysics,
+      primary: json['primary'] as bool? ?? false,
+      keyboardDismissBehavior: $enumDecodeNullable(
+              _$ScrollViewKeyboardDismissBehaviorCEnumMap,
+              json['keyboardDismissBehavior']) ??
+          ScrollViewKeyboardDismissBehaviorC.manual,
+    )
+      ..allowedTypes = (json['allowedTypes'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList()
+      ..deniedTypes = (json['deniedTypes'] as List<dynamic>)
+          .map((e) => e as String)
+          .toList()
+      ..maxAllowedSize = json['maxAllowedSize'] == null
+          ? null
+          : SizeC.fromJson(json['maxAllowedSize'] as Map)
+      ..multipleVariables = (json['multipleVariables'] as Map?)?.map(
+            (k, e) => MapEntry(k as String,
+                (e as List<dynamic>).map((e) => e as String).toList()),
+          ) ??
+          {}
+      ..isScrollable = json['isScrollable'] as bool
+      ..useFlutterListView = json['useFlutterListView'] as bool
+      ..type = json['type'] as String;
+
+Map<String, dynamic> _$PageViewNodeToJson(PageViewNode instance) {
+  final val = <String, dynamic>{
+    'id': instance.id,
+    'name': instance.name,
+    'visible': instance.visible,
+    'constraints': instance.constraints.toJson(),
+    'edgePins': instance.edgePins.toJson(),
+    'positioningMode': _$PositioningModeEnumMap[instance.positioningMode]!,
+    'horizontalFit': _$SizeFitEnumMap[instance.horizontalFit]!,
+    'verticalFit': _$SizeFitEnumMap[instance.verticalFit]!,
+    'flex': instance.flex,
+    'aspectRatioLock': instance.aspectRatioLock,
+    'reactions': instance.reactions.map((e) => e.toJson()).toList(),
+    'alignment': instance.alignment.toJson(),
+    'outerBoxLocal': instance.outerBoxLocal.toJson(),
+    'basicBoxLocal': instance.basicBoxLocal.toJson(),
+    'margin': instance.margin.toJson(),
+    'padding': instance.padding.toJson(),
+    'rotation': instance.rotationDegrees,
+    'children': instance.children,
+    'clipsContent': instance.clipsContent,
+    'allowedTypes': instance.allowedTypes,
+    'deniedTypes': instance.deniedTypes,
+  };
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('maxAllowedSize', instance.maxAllowedSize?.toJson());
+  val['variables'] = instance.variables;
+  val['multipleVariables'] = instance.multipleVariables;
+  val['isScrollable'] = instance.isScrollable;
+  val['scrollDirection'] = _$AxisCEnumMap[instance.scrollDirection]!;
+  val['reverse'] = instance.reverse;
+  val['primary'] = instance.primary;
+  val['physics'] = _$ScrollPhysicsCEnumMap[instance.physics]!;
+  val['keyboardDismissBehavior'] = _$ScrollViewKeyboardDismissBehaviorCEnumMap[
+      instance.keyboardDismissBehavior]!;
+  val['useFlutterListView'] = instance.useFlutterListView;
+  val['type'] = instance.type;
+  val['properties'] = instance.properties.toJson();
+  return val;
+}
+
+const _$SizeFitEnumMap = {
+  SizeFit.locked: 'locked',
+  SizeFit.fixed: 'fixed',
+  SizeFit.expanded: 'expanded',
+  SizeFit.flexible: 'flexible',
+  SizeFit.shrinkWrap: 'shrinkWrap',
+};
+
+const _$PositioningModeEnumMap = {
+  PositioningMode.align: 'align',
+  PositioningMode.pin: 'pin',
+};
+
+const _$AxisCEnumMap = {
+  AxisC.horizontal: 'horizontal',
+  AxisC.vertical: 'vertical',
+};
+
+const _$ScrollPhysicsCEnumMap = {
+  ScrollPhysicsC.alwaysScrollableScrollPhysics: 'alwaysScrollableScrollPhysics',
+  ScrollPhysicsC.bouncingScrollPhysics: 'bouncingScrollPhysics',
+  ScrollPhysicsC.clampingScrollPhysics: 'clampingScrollPhysics',
+  ScrollPhysicsC.neverScrollableScrollPhysics: 'neverScrollableScrollPhysics',
+  ScrollPhysicsC.rangeMaintainingScrollPhysics: 'rangeMaintainingScrollPhysics',
+};
+
+const _$ScrollViewKeyboardDismissBehaviorCEnumMap = {
+  ScrollViewKeyboardDismissBehaviorC.manual: 'manual',
+  ScrollViewKeyboardDismissBehaviorC.onDrag: 'onDrag',
+};
+
+PageViewProperties _$PageViewPropertiesFromJson(Map json) => PageViewProperties(
+      itemCount: json['itemCount'] as int?,
+      pageSnapping: json['pageSnapping'] as bool? ?? true,
+      padEnds: json['padEnds'] as bool? ?? true,
+      keepPage: json['keepPage'] as bool? ?? true,
+      initialPage: json['initialPage'] as int? ?? 0,
+      viewportFraction: (json['viewportFraction'] as num?)?.toDouble() ?? 1,
+    );
+
+Map<String, dynamic> _$PageViewPropertiesToJson(PageViewProperties instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('itemCount', instance.itemCount);
+  val['pageSnapping'] = instance.pageSnapping;
+  val['padEnds'] = instance.padEnds;
+  val['keepPage'] = instance.keepPage;
+  val['initialPage'] = instance.initialPage;
+  val['viewportFraction'] = instance.viewportFraction;
+  return val;
+}
